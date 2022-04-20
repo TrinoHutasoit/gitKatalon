b94c6586d8b1976858501a024f365a4afd41aefa
DESKTOP-UHMAS28\Trino Hutasoit <trino.hutasoit@koinworks.com>
2022-04-20 21:22:22 +0700

    object

---
+++ Object Repository/login.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>login</name>
+   <tag></tag>
+   <elementGuidId>ed374f08-8f61-46d3-9a73-03cbd842354b</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
